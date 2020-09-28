80a1d35255cfbf2b2df9690e035a31880c3c2592
Mihail51 <Mihail51@Gmail.com>
2020-09-28 03:43:28 +0300

    index.html, style.css, main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel').owlCarousel({
     items:1,
     margin:10,
-    autoHeight:true
+    autoHeight:true,
+    onInitialized: function () {
+        $('<span class="testimonials__counts">1/6</span>').prependTo('.testimonials .owl-nav')
+    }
 });
 
-owl.on('changed.owl.carousel', function(event) { 
-console.log(event)
+owl.on('changed.owl.carousel', function(event) {
+    console.log(event)
+    $('.testimonials__counts').text((event.page.index + 1) + ' / ' + event.page.count)
 })
